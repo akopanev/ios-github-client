9a64a99a209dd98f9b83006532a3ae3700ff0099
akopanev <andrew@moqod.com>
2014-10-23 13:14:41 +0400

    removed refresh control

--- GitHubClient/Classes/UI/Users/Views/GHUsersView.m
+++ GitHubClient/Classes/UI/Users/Views/GHUsersView.m
@@ -15,12 +15,13 @@
 - (instancetype)initWithFrame:(CGRect)frame {
 	if (self = [super initWithFrame:frame]) {
 		_tableView = [[UITableView alloc] initWithFrame:CGRectZero];
-		// _tableView.separatorInset = UIEdgeInsetsZero;
 		_tableView.separatorColor = [UIColor lightGrayColor];
 		[self addSubview:_tableView];
 		
+		/*
 		_refreshControl = [[UIRefreshControl alloc] init];
 		[_tableView addSubview:_refreshControl];
+		 */
 	}
 	return self;
 }
@@ -30,12 +31,6 @@
 - (void)layoutSubviews {
 	[super layoutSubviews];
 	self.tableView.frame = self.bounds;
-	
-	/*
-	CGFloat topInset = [[UIApplication sharedApplication] statusBarFrame].size.height;
-	self.tableView.contentInset = UIEdgeInsetsMake(topInset, 0.0, 0.0, 0.0);
-	self.tableView.scrollIndicatorInsets = UIEdgeInsetsMake(topInset, 0.0, 0.0, 0.0);
-	 */
 }
 
 @end
